1ac2995121a196cb9738a5b38c8fd71810dfb912
Emre Ayberk Kaymaz <emreayberk23@gmail.com>
2025-06-09 02:15:06 +0200

    feat: THM-01 rating service unit test added

--- movie-rating-service/go.mod
+++ movie-rating-service/go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/spf13/cast v1.9.2
+	github.com/stretchr/testify v1.10.0
+	github.com/swaggo/swag v1.16.4
 	golang.org/x/crypto v0.38.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.0
@@ -23,6 +25,7 @@ require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
@@ -44,13 +47,14 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.22.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/swaggo/files/v2 v2.0.2 // indirect
-	github.com/swaggo/swag v1.16.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.62.0 // indirect
 	go.opentelemetry.io/otel v1.36.0 // indirect
@@ -62,4 +66,5 @@ require (
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ movie-rating-service/internal/application/service/rating_test.go
@@ -0,0 +1,181 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"movie-rating-service/internal/application/models/request"
+	"movie-rating-service/internal/domain"
+	"movie-rating-service/mocks"
+	"testing"
+)
+
+type RatingServiceTest struct {
+	suite.Suite
+	service ratingService
+	r       *mocks.RatingRepository
+	m       *mocks.MovieRepository
+}
+
+func (r *RatingServiceTest) SetupTest() {
+	r.r = new(mocks.RatingRepository)
+	r.m = new(mocks.MovieRepository)
+
+	r.service = ratingService{ratingRepository: r.r, movieRepository: r.m}
+}
+
+func Test_RunRatingServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(RatingServiceTest))
+}
+
+func (r *RatingServiceTest) TestPromotionService_Create_Success() {
+	t := r.T()
+
+	ctx := context.TODO()
+
+	req := request.CreateRating{
+		UserID:  1,
+		MovieID: 42,
+		Score:   4.7,
+		Review:  "good",
+	}
+
+	rating := &domain.Rating{
+		UserID:  req.UserID,
+		MovieID: req.MovieID,
+		Score:   req.Score,
+		Review:  req.Review,
+	}
+
+	r.r.On("Create", ctx, mock.MatchedBy(func(r domain.Rating) bool {
+		return r.UserID == req.UserID && r.MovieID == req.MovieID && r.Score == req.Score && r.Review == req.Review
+	})).Return(rating, nil).Once()
+
+	r.m.On("UpdateRating", ctx, req.MovieID, req.Score).Return(nil).Once()
+
+	result, err := r.service.Create(ctx, req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+
+	r.r.AssertExpectations(t)
+	r.m.AssertExpectations(t)
+}
+
+func (r *RatingServiceTest) TestPromotionService_Create_Error_Failed_To_Create_Rating() {
+	t := r.T()
+
+	ctx := context.TODO()
+
+	req := request.CreateRating{
+		UserID:  1,
+		MovieID: 42,
+		Score:   4.7,
+		Review:  "good",
+	}
+
+	r.r.On("Create", ctx, mock.MatchedBy(func(r domain.Rating) bool {
+		return r.UserID == req.UserID && r.MovieID == req.MovieID && r.Score == req.Score && r.Review == req.Review
+	})).Return(nil, errors.New("there is an error")).Once()
+
+	result, err := r.service.Create(ctx, req)
+
+	assert.ErrorContains(t, err, "failed to rate movie: there is an error")
+	assert.Nil(t, result)
+
+	r.r.AssertExpectations(t)
+	r.m.AssertExpectations(t)
+}
+
+func (r *RatingServiceTest) TestPromotionService_Create_Error_Failed_To_Update_Movie() {
+	t := r.T()
+
+	ctx := context.TODO()
+
+	req := request.CreateRating{
+		UserID:  1,
+		MovieID: 42,
+		Score:   4.7,
+		Review:  "good",
+	}
+
+	rating := &domain.Rating{
+		UserID:  req.UserID,
+		MovieID: req.MovieID,
+		Score:   req.Score,
+		Review:  req.Review,
+	}
+
+	r.r.On("Create", ctx, mock.MatchedBy(func(r domain.Rating) bool {
+		return r.UserID == req.UserID && r.MovieID == req.MovieID && r.Score == req.Score && r.Review == req.Review
+	})).Return(rating, nil).Once()
+
+	r.m.On("UpdateRating", ctx, req.MovieID, req.Score).Return(errors.New("there is an error")).Once()
+
+	result, err := r.service.Create(ctx, req)
+
+	assert.ErrorContains(t, err, "failed to update rating: there is an error")
+	assert.Nil(t, result)
+
+	r.r.AssertExpectations(t)
+	r.m.AssertExpectations(t)
+}
+
+func (r *RatingServiceTest) TestPromotionService_GetRatingsByUserID_Success() {
+	t := r.T()
+
+	ctx := context.TODO()
+
+	req := request.GetUserRatings{
+		UserID: 1,
+	}
+
+	// Prepare fake ratings
+	domainRatings := []domain.Rating{
+		{
+			UserID:  req.UserID,
+			MovieID: 42,
+			Score:   4.7,
+			Review:  "good",
+		},
+		{
+			UserID:  req.UserID,
+			MovieID: 43,
+			Score:   3.2,
+			Review:  "average",
+		},
+	}
+
+	r.r.On("GetByUserID", ctx, req.UserID).Return(domainRatings, nil).Once()
+
+	result, err := r.service.GetRatingsByUserID(ctx, req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Len(t, result.Ratings, 2)
+	assert.Equal(t, domainRatings[0].Movie.Title, result.Ratings[0].RatedMovie.Title)
+	assert.Equal(t, domainRatings[1].Score, result.Ratings[1].Rating.Score)
+
+	r.r.AssertExpectations(t)
+}
+
+func (r *RatingServiceTest) TestPromotionService_GetRatingsByUserID_Error_Failed_To_Get_User_Ratings() {
+	t := r.T()
+
+	ctx := context.TODO()
+
+	req := request.GetUserRatings{
+		UserID: 1,
+	}
+
+	r.r.On("GetByUserID", ctx, req.UserID).Return(nil, errors.New("there is an error")).Once()
+
+	result, err := r.service.GetRatingsByUserID(ctx, req)
+
+	assert.ErrorContains(t, err, "error occurred while getting user's ratings: there is an error")
+	assert.Nil(t, result)
+
+	r.r.AssertExpectations(t)
+}
